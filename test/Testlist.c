42ac106c3756f6569cd14ac501d6dd0a27488498
InHavk <inhavk@gmail.com>
2020-05-09 00:07:11 +0500

    Use list_size in list tests

--- test/Testlist.c
+++ test/Testlist.c
@@ -3,7 +3,7 @@
 
 void test_create_destroy_list(void){
     List* list = list_create();
-    TEST_ASSERT_EQUAL_size_t(list->size, 0);
+    TEST_ASSERT_EQUAL_size_t(list_size(list), 0);
     TEST_ASSERT_NULL(list->el);
     list_free(list);
 }
@@ -12,13 +12,13 @@ void test_one_element_list(void){
     uint64_t* element = malloc(sizeof(uint64_t));
     *element = 10;
     List* list = list_create();
-    TEST_ASSERT_EQUAL_size_t(list->size, 0);
+    TEST_ASSERT_EQUAL_size_t(list_size(list), 0);
     TEST_ASSERT_NULL(list->el);
     list_append(list, element);
-    TEST_ASSERT_EQUAL_size_t(list->size, 1);
+    TEST_ASSERT_EQUAL_size_t(list_size(list), 1);
     TEST_ASSERT_EQUAL_UINT64(*((uint64_t*)list_get_latest(list)->value), 10);
     list_delete_latest(list);
-    TEST_ASSERT_EQUAL_size_t(list->size, 0);
+    TEST_ASSERT_EQUAL_size_t(list_size(list), 0);
     list_free(list);
 }
 
@@ -30,18 +30,18 @@ void test_some_elements_list(void){
     *el2 = 20;
     *el3 = 30;
     List* list = list_create();
-    TEST_ASSERT_EQUAL_size_t(list->size, 0);
+    TEST_ASSERT_EQUAL_size_t(list_size(list), 0);
     TEST_ASSERT_NULL(list->el);
     list_append(list, el1);
-    TEST_ASSERT_EQUAL_size_t(list->size, 1);
+    TEST_ASSERT_EQUAL_size_t(list_size(list), 1);
     TEST_ASSERT_EQUAL_UINT64(*((uint64_t*)list_get_latest(list)->value), 10);
     list_append(list, el2);
-    TEST_ASSERT_EQUAL_size_t(list->size, 2);
+    TEST_ASSERT_EQUAL_size_t(list_size(list), 2);
     TEST_ASSERT_EQUAL_UINT64(*((uint64_t*)list_get_latest(list)->value), 20);
     list_delete_latest(list);
-    TEST_ASSERT_EQUAL_size_t(list->size, 1);
+    TEST_ASSERT_EQUAL_size_t(list_size(list), 1);
     list_append(list, el3);
-    TEST_ASSERT_EQUAL_size_t(list->size, 2);
+    TEST_ASSERT_EQUAL_size_t(list_size(list), 2);
     TEST_ASSERT_EQUAL_UINT64(*((uint64_t*)list_get_latest(list)->value), 30);
     list_free(list);
 }
