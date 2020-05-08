7bc11f27694bb497dc254bcd11e033b5d00d9b4f
InHavk <inhavk@gmail.com>
2020-05-08 18:32:06 +0500

    Test nenoseconds in timestamp

--- test/Testobject.c
+++ test/Testobject.c
@@ -10,6 +10,14 @@ void test_seconds_in_timestamp(void){
     free(t);
 }
 
+void test_nanoseconds_in_timestamp(void){
+    uint64_t timestamp = generate_current_timestamp();
+    struct timespec* t = malloc(sizeof(struct timespec));
+    clock_gettime(CLOCK_REALTIME, t);
+    TEST_ASSERT(t->tv_nsec / 100 >= timestamp % 10000000);
+    free(t);
+}
+
 void setUp (void) {}
 
 void tearDown (void) {}
@@ -17,5 +25,6 @@ void tearDown (void) {}
 int main(void){
     UNITY_BEGIN();
     RUN_TEST(test_seconds_in_timestamp);
+    RUN_TEST(test_nanoseconds_in_timestamp);
     return UNITY_END();
 }
