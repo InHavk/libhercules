e67e75ab9d902bfeebb845239ee06089fa08da48
InHavk <inhavk@gmail.com>
2020-05-08 18:27:09 +0500

    Test seconds in timestamp

--- test/Testobject.c
+++ test/Testobject.c
@@ -1,9 +1,13 @@
 #include "unity.h"
 #include "object.h"
+#include <time.h>
 
-void test_timestamp(void){
+void test_seconds_in_timestamp(void){
     uint64_t timestamp = generate_current_timestamp();
-    TEST_ASSERT(timestamp > 0);
+    struct timespec* t = malloc(sizeof(struct timespec));
+    clock_gettime(CLOCK_REALTIME, t);
+    TEST_ASSERT_EQUAL_INT64(t->tv_sec, timestamp / 10000000);
+    free(t);
 }
 
 void setUp (void) {}
@@ -12,6 +16,6 @@ void tearDown (void) {}
 
 int main(void){
     UNITY_BEGIN();
-    RUN_TEST(test_timestamp);
+    RUN_TEST(test_seconds_in_timestamp);
     return UNITY_END();
 }
