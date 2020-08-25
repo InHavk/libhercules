0673131fd9cc49d35ceed61a1a4937b74d3c5429
InHavk <inhavk@gmail.com>
2020-08-25 15:31:32 +0500

    Test returning tag

--- test/Testlibhercules.c
+++ test/Testlibhercules.c
@@ -37,7 +37,7 @@ void test_event_with_container(void){
     Event* event = event_create(0x01, timestamp, uuid);
     
     // add Tag "byte" with type Byte and value 55
-    container_add_tag_Byte(event->payload, 4, "byte", 55);
+    Tag* tag1_i = container_add_tag_Byte(event->payload, 4, "byte", 55);
     // direct check
     TEST_ASSERT_EQUAL_UINT8(55, *((uint8_t*) ((Tag*) (list_get_latest(event->payload)->value))->value));
     TEST_ASSERT_EQUAL_INT8(4, ((Tag*) (list_get_latest(event->payload)->value))->key.length);
@@ -49,11 +49,17 @@ void test_event_with_container(void){
     TEST_ASSERT_EQUAL_UINT8(55, tag_get_Byte(tag1));
     TEST_ASSERT_EQUAL_INT8(4, tag1->key.length);
     TEST_ASSERT_EQUAL_STRING("byte", tag1->key.value);
+    // return check
+    TEST_ASSERT_NOT_NULL(tag1_i);
+    TEST_ASSERT_NOT_NULL(tag1_i->value);
+    TEST_ASSERT_EQUAL_UINT8(55, tag_get_Byte(tag1_i));
+    TEST_ASSERT_EQUAL_INT8(4, tag1_i->key.length);
+    TEST_ASSERT_EQUAL_STRING("byte", tag1_i->key.value);
     // type check
     TEST_ASSERT_EQUAL(BYTE, tag1->datatype);
 
     // add Tag "short" with type Short and value 1024
-    container_add_tag_Short(event->payload, 5, "short", 1024);
+    Tag* tag2_i = container_add_tag_Short(event->payload, 5, "short", 1024);
     // direct check
     TEST_ASSERT_EQUAL_INT16(1024, *((int16_t*) ((Tag*) (list_get_latest(event->payload)->value))->value));
     TEST_ASSERT_EQUAL_INT8(5, ((Tag*) (list_get_latest(event->payload)->value))->key.length);
@@ -65,11 +71,17 @@ void test_event_with_container(void){
     TEST_ASSERT_EQUAL_INT16(1024, tag_get_Short(tag2));
     TEST_ASSERT_EQUAL_INT8(5, tag2->key.length);
     TEST_ASSERT_EQUAL_STRING("short", tag2->key.value);
+    // return check
+    TEST_ASSERT_NOT_NULL(tag2_i);
+    TEST_ASSERT_NOT_NULL(tag2_i->value);
+    TEST_ASSERT_EQUAL_INT16(1024, tag_get_Short(tag2_i));
+    TEST_ASSERT_EQUAL_INT8(5, tag2_i->key.length);
+    TEST_ASSERT_EQUAL_STRING("short", tag2_i->key.value);
     // type check
     TEST_ASSERT_EQUAL(SHORT, tag2->datatype);
 
     // add Tag "integer" with type Integer and value 1048576
-    container_add_tag_Integer(event->payload, 7, "integer", 1048576);
+    Tag* tag3_i = container_add_tag_Integer(event->payload, 7, "integer", 1048576);
     // direct check
     TEST_ASSERT_EQUAL_INT32(1048576, *((int32_t*) ((Tag*) (list_get_latest(event->payload)->value))->value));
     TEST_ASSERT_EQUAL_INT8(7, ((Tag*) (list_get_latest(event->payload)->value))->key.length);
@@ -81,11 +93,17 @@ void test_event_with_container(void){
     TEST_ASSERT_EQUAL_INT32(1048576, tag_get_Integer(tag3));
     TEST_ASSERT_EQUAL_INT8(7, tag3->key.length);
     TEST_ASSERT_EQUAL_STRING("integer", tag3->key.value);
+    // return check
+    TEST_ASSERT_NOT_NULL(tag3_i);
+    TEST_ASSERT_NOT_NULL(tag3_i->value);
+    TEST_ASSERT_EQUAL_INT32(1048576, tag_get_Integer(tag3_i));
+    TEST_ASSERT_EQUAL_INT8(7, tag3_i->key.length);
+    TEST_ASSERT_EQUAL_STRING("integer", tag3_i->key.value);
     // type check
     TEST_ASSERT_EQUAL(INTEGER, tag3->datatype);
 
     // add Tag "long" with type Long and value 274877906944
-    container_add_tag_Long(event->payload, 4, "long", 274877906944);
+    Tag* tag4_i = container_add_tag_Long(event->payload, 4, "long", 274877906944);
     // direct check
     TEST_ASSERT_EQUAL_INT64(274877906944, *((int64_t*) ((Tag*) (list_get_latest(event->payload)->value))->value));
     TEST_ASSERT_EQUAL_INT8(4, ((Tag*) (list_get_latest(event->payload)->value))->key.length);
@@ -97,11 +115,17 @@ void test_event_with_container(void){
     TEST_ASSERT_EQUAL_INT64(274877906944, tag_get_Long(tag4));
     TEST_ASSERT_EQUAL_INT8(4, tag4->key.length);
     TEST_ASSERT_EQUAL_STRING("long", tag4->key.value);
+    // return check
+    TEST_ASSERT_NOT_NULL(tag4_i);
+    TEST_ASSERT_NOT_NULL(tag4_i->value);
+    TEST_ASSERT_EQUAL_INT64(274877906944, tag_get_Long(tag4_i));
+    TEST_ASSERT_EQUAL_INT8(4, tag4_i->key.length);
+    TEST_ASSERT_EQUAL_STRING("long", tag4_i->key.value);
     // type check
     TEST_ASSERT_EQUAL(LONG, tag4->datatype);
 
     // add Tag "flag" with type Flag and value 1
-    container_add_tag_Flag(event->payload, 4, "flag", 1);
+    Tag* tag5_i = container_add_tag_Flag(event->payload, 4, "flag", 1);
     // direct check
     TEST_ASSERT_EQUAL(1, *((char*) ((Tag*) (list_get_latest(event->payload)->value))->value));
     TEST_ASSERT_EQUAL_INT8(4, ((Tag*) (list_get_latest(event->payload)->value))->key.length);
@@ -113,11 +137,17 @@ void test_event_with_container(void){
     TEST_ASSERT_EQUAL(1, tag_get_Flag(tag5));
     TEST_ASSERT_EQUAL_INT8(4, tag5->key.length);
     TEST_ASSERT_EQUAL_STRING("flag", tag5->key.value);
+    // return check
+    TEST_ASSERT_NOT_NULL(tag5_i);
+    TEST_ASSERT_NOT_NULL(tag5_i->value);
+    TEST_ASSERT_EQUAL(1, tag_get_Flag(tag5_i));
+    TEST_ASSERT_EQUAL_INT8(4, tag5_i->key.length);
+    TEST_ASSERT_EQUAL_STRING("flag", tag5_i->key.value);
     // type check
     TEST_ASSERT_EQUAL(FLAG, tag5->datatype);
 
     // add Tag "float" with type Float and value 32.0f
-    container_add_tag_Float(event->payload, 5, "float", 32.0f);
+    Tag* tag6_i = container_add_tag_Float(event->payload, 5, "float", 32.0f);
     // direct check
     TEST_ASSERT_EQUAL_FLOAT(32.0f, *((float*) ((Tag*) (list_get_latest(event->payload)->value))->value));
     TEST_ASSERT_EQUAL_INT8(5, ((Tag*) (list_get_latest(event->payload)->value))->key.length);
@@ -129,11 +159,17 @@ void test_event_with_container(void){
     TEST_ASSERT_EQUAL_FLOAT(32.0f, tag_get_Float(tag6));
     TEST_ASSERT_EQUAL_INT8(5, tag6->key.length);
     TEST_ASSERT_EQUAL_STRING("float", tag6->key.value);
+    // return check
+    TEST_ASSERT_NOT_NULL(tag6_i);
+    TEST_ASSERT_NOT_NULL(tag6_i->value);
+    TEST_ASSERT_EQUAL_FLOAT(32.0f, tag_get_Float(tag6_i));
+    TEST_ASSERT_EQUAL_INT8(5, tag6_i->key.length);
+    TEST_ASSERT_EQUAL_STRING("float", tag6_i->key.value);
     // type check
     TEST_ASSERT_EQUAL(FLOAT, tag6->datatype);
 
     // add Tag "double" with type Double and value 64.0
-    container_add_tag_Double(event->payload, 6, "double", 64.0);
+    Tag* tag7_i = container_add_tag_Double(event->payload, 6, "double", 64.0);
     // direct check
     TEST_ASSERT_EQUAL(64.0, *((double*) ((Tag*) (list_get_latest(event->payload)->value))->value));
     TEST_ASSERT_EQUAL_INT8(6, ((Tag*) (list_get_latest(event->payload)->value))->key.length);
@@ -145,11 +181,17 @@ void test_event_with_container(void){
     TEST_ASSERT_EQUAL(64.0, tag_get_Double(tag7));
     TEST_ASSERT_EQUAL_INT8(6, tag7->key.length);
     TEST_ASSERT_EQUAL_STRING("double", tag7->key.value);
+    // return check
+    TEST_ASSERT_NOT_NULL(tag7_i);
+    TEST_ASSERT_NOT_NULL(tag7_i->value);
+    TEST_ASSERT_EQUAL(64.0, tag_get_Double(tag7_i));
+    TEST_ASSERT_EQUAL_INT8(6, tag7_i->key.length);
+    TEST_ASSERT_EQUAL_STRING("double", tag7_i->key.value);
     // type check
     TEST_ASSERT_EQUAL(DOUBLE, tag7->datatype);
 
     // add Tag "string" with type String and value "test string"
-    container_add_tag_String(event->payload, 6, "string", "test string");
+    Tag* tag8_i = container_add_tag_String(event->payload, 6, "string", "test string");
     // direct check
     TEST_ASSERT_EQUAL_STRING("test string", (char*) ((Tag*) (list_get_latest(event->payload)->value))->value);
     TEST_ASSERT_EQUAL_INT8(6, ((Tag*) (list_get_latest(event->payload)->value))->key.length);
@@ -161,12 +203,18 @@ void test_event_with_container(void){
     TEST_ASSERT_EQUAL_STRING("test string", tag_get_String(tag8));
     TEST_ASSERT_EQUAL_INT8(6, tag8->key.length);
     TEST_ASSERT_EQUAL_STRING("string", tag8->key.value);
+    // return check
+    TEST_ASSERT_NOT_NULL(tag8_i);
+    TEST_ASSERT_NOT_NULL(tag8_i->value);
+    TEST_ASSERT_EQUAL_STRING("test string", tag_get_String(tag8_i));
+    TEST_ASSERT_EQUAL_INT8(6, tag8_i->key.length);
+    TEST_ASSERT_EQUAL_STRING("string", tag8_i->key.value);
     // type check
     TEST_ASSERT_EQUAL(STRING, tag8->datatype);
 
     // add Tag "uuid" with type UUID and value 0x5f, 0x3e, 0x88, 0xbb, 0x23, 0xf0, 0xfe, 0x97, 0xcb, 0xc5, 0xe2, 0x3d, 0x82, 0xdd, 0x3e, 0xca
     uint8_t uuid_value[16] = {0x5f, 0x3e, 0x88, 0xbb, 0x23, 0xf0, 0xfe, 0x97, 0xcb, 0xc5, 0xe2, 0x3d, 0x82, 0xdd, 0x3e, 0xca};
-    container_add_tag_UUID(event->payload, 4, "uuid", uuid_value);
+    Tag* tag9_i = container_add_tag_UUID(event->payload, 4, "uuid", uuid_value);
     // direct check
     TEST_ASSERT_EQUAL_UINT8_ARRAY(uuid_value, (uint8_t*) ((Tag*) (list_get_latest(event->payload)->value))->value, 16);
     TEST_ASSERT_EQUAL_INT8(4, ((Tag*) (list_get_latest(event->payload)->value))->key.length);
@@ -178,26 +226,35 @@ void test_event_with_container(void){
     TEST_ASSERT_EQUAL_UINT8_ARRAY(uuid_value, tag_get_UUID(tag9), 16);
     TEST_ASSERT_EQUAL_INT8(4, tag9->key.length);
     TEST_ASSERT_EQUAL_STRING("uuid", tag9->key.value);
+    // return check
+    TEST_ASSERT_NOT_NULL(tag9_i);
+    TEST_ASSERT_NOT_NULL(tag9_i->value);
+    TEST_ASSERT_EQUAL_UINT8_ARRAY(uuid_value, tag_get_UUID(tag9_i), 16);
+    TEST_ASSERT_EQUAL_INT8(4, tag9_i->key.length);
+    TEST_ASSERT_EQUAL_STRING("uuid", tag9_i->key.value);
     // type check
     TEST_ASSERT_EQUAL(UUID, tag9->datatype);
 
     // add Tag "null" with type Null
-    container_add_tag_Null(event->payload, 4, "null");
+    Tag* tag10_i = container_add_tag_Null(event->payload, 4, "null");
     // direct check
     TEST_ASSERT_EQUAL_INT8(4, ((Tag*) (list_get_latest(event->payload)->value))->key.length);
     TEST_ASSERT_EQUAL_STRING("null", ((Tag*) (list_get_latest(event->payload)->value))->key.value);
     // normal check
     Tag* tag10 = container_find_tag(event->payload, 4, "null");
     TEST_ASSERT_NOT_NULL(tag10);
-    TEST_ASSERT_NOT_NULL(tag10->value);
     TEST_ASSERT_EQUAL_INT8(4, tag10->key.length);
     TEST_ASSERT_EQUAL_STRING("null", tag10->key.value);
+    // return check
+    TEST_ASSERT_NOT_NULL(tag10_i);
+    TEST_ASSERT_EQUAL_INT8(4, tag10_i->key.length);
+    TEST_ASSERT_EQUAL_STRING("null", tag10_i->key.value);
     // type check
     TEST_ASSERT_EQUAL(NULL_TYPE, tag10->datatype);
 
 
     // add Tag "vector" with type Vector
-    container_add_tag_Vector(event->payload, NULL_TYPE, 6, "vector");
+    Tag* tag11_i = container_add_tag_Vector(event->payload, NULL_TYPE, 6, "vector");
     // direct check
     TEST_ASSERT_EQUAL_size_t(0, (size_t) ((List*)((Vector*)((Tag*) (list_get_latest(event->payload)->value))->value)->values)->size);
     TEST_ASSERT_NULL((List_element*)((List*)((Vector*)((Tag*) (list_get_latest(event->payload)->value))->value)->values)->el);
@@ -213,11 +270,19 @@ void test_event_with_container(void){
     TEST_ASSERT_EQUAL(NULL_TYPE, tag_get_Vector(tag11)->datatype);
     TEST_ASSERT_EQUAL_INT8(6, tag11->key.length);
     TEST_ASSERT_EQUAL_STRING("vector", tag11->key.value);
+    // return check
+    TEST_ASSERT_NOT_NULL(tag11_i);
+    TEST_ASSERT_NOT_NULL(tag11_i->value);
+    TEST_ASSERT_EQUAL_size_t(0, tag_get_Vector(tag11_i)->values->size);
+    TEST_ASSERT_NULL(tag_get_Vector(tag11_i)->values->el);
+    TEST_ASSERT_EQUAL(NULL_TYPE, tag_get_Vector(tag11_i)->datatype);
+    TEST_ASSERT_EQUAL_INT8(6, tag11_i->key.length);
+    TEST_ASSERT_EQUAL_STRING("vector", tag11_i->key.value);
     // type check
     TEST_ASSERT_EQUAL(VECTOR, tag11->datatype);
 
     // add Tag "container" with type Container
-    container_add_tag_Container(event->payload, 9, "container");
+    Tag* tag12_i = container_add_tag_Container(event->payload, 9, "container");
     // direct check
     TEST_ASSERT_EQUAL_size_t(0, (size_t) ((List*) ((Tag*) (list_get_latest(event->payload)->value))->value)->size);
     TEST_ASSERT_NULL((List_element*) ((List*) ((Tag*) (list_get_latest(event->payload)->value))->value)->el);
@@ -231,6 +296,13 @@ void test_event_with_container(void){
     TEST_ASSERT_NULL(tag_get_Container(tag12)->el);
     TEST_ASSERT_EQUAL_INT8(9, tag12->key.length);
     TEST_ASSERT_EQUAL_STRING("container", tag12->key.value);
+    // return check
+    TEST_ASSERT_NOT_NULL(tag12_i);
+    TEST_ASSERT_NOT_NULL(tag12_i->value);
+    TEST_ASSERT_EQUAL_size_t(0, tag_get_Container(tag12_i)->size);
+    TEST_ASSERT_NULL(tag_get_Container(tag12_i)->el);
+    TEST_ASSERT_EQUAL_INT8(9, tag12_i->key.length);
+    TEST_ASSERT_EQUAL_STRING("container", tag12_i->key.value);
     // type check
     TEST_ASSERT_EQUAL(CONTAINER, tag12->datatype);
 
